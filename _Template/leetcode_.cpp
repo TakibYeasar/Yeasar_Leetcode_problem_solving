530fe66b2ca9e4d65f66a887a5d78030249a66fb
Takib Yeasar <mdtakibyeasar@gmail.com>
2025-04-05 19:51:07 +0600

    updated leetcode templates

---
+++ _Template/leetcode_.cpp
@@ -0,0 +1,44 @@
+
+// Problem link ===>>
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long
+#define endl '\n'
+// Constants
+const int INF = 0x3f3f3f3f;
+const long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_set_mul = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename V>
+using ordered_map = tree<
+    K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename V>
+using ordered_map_mul = tree<
+    K, V, less_equal<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        
+    }
+    return 0;
+}
---
+++ _Template/leetcode_.py
@@ -0,0 +1,29 @@
+
+# Problem link ===>> 
+# submission link == =>> 
+
+import sys
+import math
+import bisect
+import heapq
+from collections import defaultdict, deque, OrderedDict, Counter
+from functools import lru_cache, reduce
+from itertools import permutations, combinations, accumulate
+from typing import List, Tuple, Dict, Set, Optional, Union, Any
+
+MOD = 10**9 + 7
+INF = float('inf')
+input = sys.stdin.read
+print = sys.stdout.write
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        pass
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+
+if __name__ == "__main__":
+    solution = Solution()
